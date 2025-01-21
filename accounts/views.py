30c541bca428e9d5ea17922fb0f93cfc5fc2e9ce
Sthewerson <fernandoo-rocha@hotmail.com>
2025-01-21 13:51:59 -0300

    feat: create UserView for user profile management

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import APIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.exceptions import AuthenticationFailed
@@ -15,7 +15,10 @@ from accounts.models import User
 
 from core.utils.exceptions import ValidationError
 
-class SignInView(APIView,Authentication):
+import uuid
+
+
+class SignInView(APIView, Authentication):
     permission_classes = [AllowAny]
 
     def post(self, request):
@@ -27,13 +30,14 @@ class SignInView(APIView,Authentication):
         if not signin:
             raise AuthenticationFailed
 
-        user = UserSerializer(signin.user).data
-        access_token = RefreshToken.for_user(signin.user).access_token
+        user = UserSerializer(signin).data
+        access_token = RefreshToken.for_user(signin).access_token
 
         return Response({
-            'user': user,
-            'access_token': str(access_token)
+            "user": user,
+            "access_token": str(access_token)
         })
+
 
 class SignUpView(APIView, Authentication):
     permission_classes = [AllowAny]
@@ -55,6 +59,72 @@ class SignUpView(APIView, Authentication):
         access_token = RefreshToken.for_user(signup).access_token
 
         return Response({
-            'user': user,
-            'access_token': str(access_token)
+            "user": user,
+            "access_token": str(access_token)
+        })
+
+
+class UserView(APIView):
+    def get(self, request):
+        # Update last_access
+        User.objects.filter(id=request.user.id).update(last_access=now())
+
+        user = UserSerializer(request.user).data
+
+        return Response({
+            "user": user
+        })
+
+    def put(self, request):
+        name = request.data.get('name')
+        email = request.data.get('email')
+        password = request.data.get('password')
+        avatar = request.FILES.get('avatar')
+
+        # Initialize storage
+        storage = FileSystemStorage(
+            settings.MEDIA_ROOT / "avatars",
+            settings.MEDIA_URL + "avatars"
+        )
+
+        if avatar:
+            content_type = avatar.content_type
+            extension = avatar.name.split('.')[-1]
+
+            # Validate avatar
+            if not content_type == "image/png" and not content_type == "image/jpeg":
+                raise ValidationError(
+                    "Somente arquivos do tipo PNG ou JPEG são suportados")
+
+            # Save new avatar
+            file = storage.save(f"{uuid.uuid4()}.{extension}", avatar)
+            avatar = storage.url(file)
+
+        serializer = UserSerializer(request.user, data={
+            "name": name,
+            "email": email,
+            "avatar": avatar or request.user.avatar
+        })
+
+        if not serializer.is_valid():
+            # Delete uploaded file
+            if avatar:
+                storage.delete(avatar.split("/")[-1])
+
+            first_error = list(serializer.errors.values())[0][0]
+
+            raise ValidationError(first_error)
+
+        # Delete old avatar
+        if avatar and request.user.avatar != "/media/avatars/default-avatar.png":
+            storage.delete(request.user.avatar.split("/")[-1])
+
+        # Update password
+        if password:
+            request.user.set_password(password)
+
+        serializer.save()
+
+        return Response({
+            "user": serializer.data
         })
